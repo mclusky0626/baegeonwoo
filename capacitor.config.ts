5de80c0ad10c0cb632aaf077178fc524474cdf92
배건우 <102368729+mclusky0626@users.noreply.github.com>
2025-07-05 15:18:04 +0900

    안드로이드 배포
    cd

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mclusky.dagub',
+  appName: 'dagub',
+  webDir: 'dist'
+};
+
+export default config;
