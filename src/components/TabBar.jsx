9746f26c805d0a7df2ba89e5eb1680f4e1720909
배건우 <102368729+mclusky0626@users.noreply.github.com>
2025-08-31 17:29:59 +0900

    Update TabBar.jsx

--- src/components/TabBar.jsx
+++ src/components/TabBar.jsx
@@ -3,8 +3,8 @@ import "./TabBar.css";
 
 export const TabBar = () => {
   const tabs = [
-    { key: "home", path: "/", icon: { active: "home1.svg", inactive: "home0.svg" }, label: "홈" },
-    { key: "week", path: "/week", icon: { active: "calendar1.svg", inactive: "calendar0.svg" }, label: "급식표" },
+    { key: "home", path: "/", icon: { active: "home0.svg", inactive: "home1.svg" }, label: "홈" },
+    { key: "week", path: "/week", icon: { active: "calendar0.svg", inactive: "calendar1.svg" }, label: "급식표" },
     { key: "feedback", path: "/feedback", icon: { active: "message-square-blue.svg", inactive: "message-square-gray.svg" }, label: "피드백" },
     { key: "settings", path: "/settings", icon: { active: "settings1.svg", inactive: "settings0.svg" }, label: "설정" },
     { key: "frame", path: "/frame", icon: { active: "user1.svg", inactive: "user0.svg" }, label: "내정보" },
